d66ca4b3f909c27db033fe16ce3cb8fedd289b34
Sebastian Springer <info@sebastian-springer.com>
2019-03-21 14:53:45 +0100

    swagger

---
+++ quiz/src/quiz/quiz.interface.ts
@@ -0,0 +1,7 @@
+export interface Quiz {
+  id: number;
+  title: string;
+  answer1: string;
+  answer2: string;
+  correct?: number;
+}
--- quiz/src/quiz/quiz/quiz.controller.ts
+++ quiz/src/quiz/quiz/quiz.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Quiz } from '../quiz.interface';
 
 @Controller('quiz')
 export class QuizController {
-  private quiz = [
+  private quiz: Quiz[] = [
     {
       id: 1,
       title: 'Wieviel ist 1 + 1',
@@ -13,14 +14,14 @@ export class QuizController {
   ];
 
   @Get(':id')
-  getQuiz(@Param() param) {
-    const quiz = this.quiz.find(q => q.id === parseInt(param.id, 10));
+  getQuiz(@Param('id') id: string): Quiz {
+    const quiz = this.quiz.find(q => q.id === parseInt(id, 10));
     delete quiz.correct;
     return quiz;
   }
 
   @Post('solve/:id')
-  solveQuiz(@Body() body: { answer: number }, @Param() param) {
+  solveQuiz(@Body() body: { answer: number }, @Param() param): boolean {
     const quiz = this.quiz.find(q => q.id === parseInt(param.id, 10));
     if (body.answer === quiz.correct) {
       return true;

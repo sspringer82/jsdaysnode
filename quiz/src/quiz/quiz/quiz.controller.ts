28ec89d30c5865e77025e8f0be53ed8dbc11d46d
Sebastian Springer <info@sebastian-springer.com>
2019-03-21 14:41:17 +0100

    solve

--- quiz/src/quiz/quiz/quiz.controller.ts
+++ quiz/src/quiz/quiz/quiz.controller.ts
@@ -1,9 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('quiz')
 export class QuizController {
-  @Get()
-  getQuiz() {
-    return { title: 'Frage', answer1: 'a', answer2: 'b', correct: 2 };
+  private quiz = [
+    {
+      id: 1,
+      title: 'Wieviel ist 1 + 1',
+      answer1: '1',
+      answer2: '2',
+      correct: 2,
+    },
+  ];
+
+  @Get(':id')
+  getQuiz(@Param() param) {
+    const quiz = this.quiz.find(q => q.id === parseInt(param.id, 10));
+    delete quiz.correct;
+    return quiz;
+  }
+
+  @Post('solve/:id')
+  solveQuiz(@Body() body: { answer: number }, @Param() param) {
+    const quiz = this.quiz.find(q => q.id === parseInt(param.id, 10));
+    if (body.answer === quiz.correct) {
+      return true;
+    }
+    return false;
   }
 }
